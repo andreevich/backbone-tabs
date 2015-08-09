776d1052fdbbd78f7206eb2b850156a195d43945
andreevich <5316552@gmail.com>
2015-08-09 15:30:33 +0300

    add new

---
+++ js/app.js
@@ -0,0 +1,24 @@
+﻿var app = app || {}
+
+$(function(){
+	
+	app.tabs = new app.Tabs([
+		{name:"reload", text:'&nbsp;§&nbsp;',isblocked:true},
+		{name:"insert", text:'Добавление',isactive:'active',content:'Добавление чего-то там'},
+		{name:"report",text:"Отчёт",content:'Отчёты...'},
+		{name:"help",text:"?",content:'Справка'},
+		{name:"user", text:'Пользователь ',isblocked:true}
+	])
+
+	new app.TabsView();
+	
+	app.tabs.get('report').set({content:"Новый отчёт"})
+	
+	/*
+		Добавляю функционала кнопке. Переопределяю функцию пустую f()
+	*/
+	app.tabs.get('reload').f=function(){
+		location.reload()
+	}
+
+})
---
+++ js/lib/Tabs.js
@@ -0,0 +1,96 @@
+﻿var app = app || {};
+
+
+(function () {
+	'use strict';
+	
+/*
+	модель таба
+*/
+	app.TabModel = Backbone.Model.extend({
+		idAttribute: 'name',
+		defaults: {
+			name:'',							// связи таба и вкладки. быстрый доступ по имени
+			text: 'Tab',						// наименование таба
+			isblocked:false,  			// залоченый таб без вкладки
+			content:"tab's content", 	// содержимое вкладки
+			isactive:'' ,						// активный таб isactive:'active' )
+		},
+		validate:function(){
+			if (this.isblocked){
+				console.log(this)
+				return "-"
+			}
+		},
+		f:function(){}					//вызов пустой функции по клику на таб. Переопределяется в основном приложении
+	});
+/*
+	колекция табов
+*/	
+	app.Tabs = Backbone.Collection.extend({
+		model: app.TabModel,
+		initialize: function(models) {},
+		getDataByName: function(name){
+			var temp = ""
+			this.each(function(a){
+				if (a.get('name')==name){
+					temp = a;
+					return;
+				}
+			})
+			return temp
+		}
+	});
+
+	/*
+		Вьюха для таба
+	*/
+	app.TabitemView = Backbone.View.extend({
+		template: _.template($('#tabitem-template').html()),
+		events:{
+			'click': 'click'
+		},
+		initialize: function () {
+		 	this.render()
+		},
+		click:function(){
+			if (!this.model.get("isblocked")){
+				app.tabs.each(function(el){
+					if (!el.get("isblocked"))
+						el.set({isactive:''})
+				})
+				this.model.set({isactive:'active'})
+			}
+			this.model.f()
+		},
+		
+		render: function () {
+			this.setElement(this.template(this.model.toJSON()));
+			return this;
+		}
+		});
+
+	/*
+		Вьюха для списка табов 
+	*/
+	
+	app.TabsView = Backbone.View.extend({
+		el: "#tabs",
+		initialize: function(){
+			this.render()
+			this.listenTo(app.tabs, "change", this.render);
+		},
+		render: function(){
+			$('#tabs, #tabs_').html('')
+			app.tabs.each(function(item){
+				var tabitemView = new app.TabitemView({ model: item });
+				this.$el.append(tabitemView.el)
+				
+				var template = _.template($('#tabitem_-template_').html());
+				$('#tabs_').append(template(item.toJSON()));
+			},this);
+		}
+	});
+	
+	
+})();
